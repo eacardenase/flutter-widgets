3e232a093b4ac18f363d86b7ed8d0559ab29ac38
Edwin Cardenas <eacardenase@gmail.com>
2024-03-11 12:18:56 -0500

    feat: add app theme and color scheme seeds

---
+++ lib/config/theme/app_theme.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+const _colors = <Color>[
+  Colors.blue,
+  Colors.teal,
+  Colors.green,
+  Colors.red,
+  Colors.purple,
+  Colors.deepPurple,
+  Colors.orange,
+  Colors.pink,
+  Colors.pinkAccent,
+];
+
+class AppTheme {
+  final int selectedColor;
+
+  AppTheme({
+    this.selectedColor = 0,
+  }) : assert(
+          selectedColor >= 0 && selectedColor < _colors.length,
+          "Must be between 0 and ${_colors.length - 1}",
+        );
+
+  ThemeData getTheme() => ThemeData(
+        colorSchemeSeed: _colors[selectedColor],
+      );
+}
