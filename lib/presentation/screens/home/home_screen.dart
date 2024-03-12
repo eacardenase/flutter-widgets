38fa4ff3addffc673221f1b553c466487b329278
Edwin Cardenas <eacardenase@gmail.com>
2024-03-12 11:04:52 -0500

    refactor: named routes

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -3,8 +3,11 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
 import 'package:widgets_app/config/menu/menu_items.dart';
+import 'package:widgets_app/presentation/screens/cards/cards_screen.dart';
 
 class HomeScreen extends StatelessWidget {
+  static const name = 'home_screen';
+
   const HomeScreen({super.key});
 
   @override
@@ -56,6 +59,7 @@ class _CustomListTile extends StatelessWidget {
         Icons.chevron_right,
         color: colors.primary,
       ),
+      // onTap: () => context.pushNamed(CardsScreen.name),
       onTap: () => context.push(menuItem.link),
     );
   }
